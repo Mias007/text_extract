e04a0fdf059fed0eb88929f3bbbfbb645c98bbd5
Joella45 <rasoanandrasanajoella62@gmail.com>
2025-12-01 09:51:31 +0300

    Fix STATIC_ROOT for Render

--- text_extract/settings.py
+++ text_extract/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+ALLOWED_HOSTS = ['*']
